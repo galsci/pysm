8b190cf81dc5ed38862f35f60a4a077126eb7489
Andrea Zonca <code@andreazonca.com>
2019-01-09 10:44:59 -0800

    simplified unit test for units in dust with pytest

--- pysm/tests/test_dust.py
+++ pysm/tests/test_dust.py
@@ -1,116 +1,33 @@
-import unittest
-import numpy as np
+import pytest
 import pysm.component_models.galactic.dust as dust
 import astropy.units as units
 from astropy.units import UnitsError
 
 
-class TestModifiedBlackBody(unittest.TestCase):
+def test_blackbody_ratio():
+    nu_from = 100. * units.GHz
+    nu_to = 400. * units.GHz
+    temp = 20. * units.K
 
-    def setUp(self):
-        return
+    nu_from_unitless = 100.
+    nu_to_unitless = 400.
+    temp_unitless = 20.
 
-    def tearDown(self):
-        return
+    nu_from_wrong_unit = 100. * units.K
+    nu_to_wrong_unit = 400. * units.K
+    temp_wrong_unit = 20. * units.s
 
-    def test_emission(self):
-        return
-
-
-class TestDecorrelatedModifiedBlackBody(unittest.TestCase):
-
-    def setUp(self):
-        return
-
-    def tearDown(self):
-        return
-
-    def test_emission(self):
-        return
-
-
-class TestDecorrelationMatrices(unittest.TestCase):
-
-    def setUp(self):
-        return
-
-    def tearDown(self):
-        return
-
-
-class TestInvertSafe(unittest.TestCase):
-
-    def setUp(self):
-        return
-
-    def tearDown(self):
-        return
-
-    def test_inversion(self):
-        return
-
-
-class TestBlackbodyRatio(unittest.TestCase):
-
-    def setUp(self):
-        self.nu_from = 100. * units.GHz
-        self.nu_to = 400. * units.GHz
-        self.temp = 20. * units.K
-
-        self.nu_from_unitless = 100.
-        self.nu_to_unitless = 400.
-        self.temp_unitless = 20.
-
-        self.nu_from_wrong_unit = 100. * units.K
-        self.nu_to_wrong_unit = 400. * units.K
-        self.temp_wrong_unit = 20. * units.s
-
-    def test_blackbody_ratio(self):
-        dust.blackbody_ratio(self.nu_to, self.nu_from, self.temp)
-        dust.blackbody_ratio(self.nu_to, self.nu_from, self.temp)
-        self.assertRaises(
-            UnitsError,
-            dust.blackbody_ratio,
-            self.nu_to,
-            self.nu_from_wrong_unit,
-            self.temp,
-        )
-        self.assertRaises(
-            TypeError,
-            dust.blackbody_ratio,
-            self.nu_to,
-            self.nu_from_unitless,
-            self.temp,
-        )
-        self.assertRaises(
-            units.UnitsError,
-            dust.blackbody_ratio,
-            self.nu_to_wrong_unit,
-            self.nu_from,
-            self.temp,
-        )
-        self.assertRaises(
-            TypeError,
-            dust.blackbody_ratio,
-            self.nu_to_unitless,
-            self.nu_from,
-            self.temp,
-        )
-        self.assertRaises(
-            units.UnitsError,
-            dust.blackbody_ratio,
-            self.nu_to,
-            self.nu_from,
-            self.temp_wrong_unit,
-        )
-        self.assertRaises(
-            TypeError,
-            dust.blackbody_ratio,
-            self.nu_to,
-            self.nu_from,
-            self.temp_unitless,
-        )
-
-
-if __name__ == "__main__":
-    unittest.main()
+    dust.blackbody_ratio(nu_to, nu_from, temp)
+    dust.blackbody_ratio(nu_to, nu_from, temp)
+    with pytest.raises(UnitsError):
+        dust.blackbody_ratio(nu_to, nu_from_wrong_unit, temp)
+    with pytest.raises(TypeError):
+        dust.blackbody_ratio(nu_to, nu_from_unitless, temp)
+    with pytest.raises(units.UnitsError):
+        dust.blackbody_ratio(nu_to_wrong_unit, nu_from, temp)
+    with pytest.raises(TypeError):
+        dust.blackbody_ratio(nu_to_unitless, nu_from, temp)
+    with pytest.raises(units.UnitsError):
+        dust.blackbody_ratio(nu_to, nu_from, temp_wrong_unit)
+    with pytest.raises(TypeError):
+        dust.blackbody_ratio(nu_to, nu_from, temp_unitless)
