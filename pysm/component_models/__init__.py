dc9210c7bff43860cab5fd054ef052eaca638358
Andrea Zonca <code@andreazonca.com>
2019-01-08 23:46:22 -0800

    import read_map in base package

--- pysm/component_models/__init__.py
+++ pysm/component_models/__init__.py
@@ -1,3 +1,3 @@
 from .galactic import ModifiedBlackBody, DecorrelatedModifiedBlackBody, SynchrotronPowerLaw
 from .extragalactic import InterpolatedCIB
-from .template import Model
+from .template import Model, read_map
