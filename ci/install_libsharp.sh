f0f434ca5299453107c897d6bbdf6ece602e5420
Andrea Zonca <code@andreazonca.com>
2019-02-26 16:10:07 -0800

    [ci] install libsharp on travis

---
+++ ci/install_libsharp.sh
@@ -0,0 +1,11 @@
+export LIBSHARP=~/miniconda
+git clone https://github.com/Libsharp/libsharp --branch v1.0.0 --single-branch --depth 1 \
+    && cd libsharp \
+    && autoreconf \
+    && CC="mpicc" \
+    ./configure --enable-mpi --enable-pic \
+    && make \
+    && cp -a auto/* $LIBSHARP \
+    && cd python \
+    && CC="mpicc -g" LDSHARED="mpicc -g -shared" \
+    pip install .
