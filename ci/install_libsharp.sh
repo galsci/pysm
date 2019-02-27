0ea5bba774b2e53e8042e61a4f29d00b899c4c5f
Andrea Zonca <code@andreazonca.com>
2019-02-26 18:59:30 -0800

    [ci] install mpi4py on travis with conda

--- ci/install_libsharp.sh
+++ ci/install_libsharp.sh
@@ -4,7 +4,7 @@ git clone https://github.com/Libsharp/libsharp --branch v1.0.0 --single-branch -
     && autoreconf \
     && CC="mpicc" \
     ./configure --enable-mpi --enable-pic \
-    && make \
+    && make -j4 \
     && cp -a auto/* $LIBSHARP \
     && cd python \
     && CC="mpicc -g" LDSHARED="mpicc -g -shared" \
