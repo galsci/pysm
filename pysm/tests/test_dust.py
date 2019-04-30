4e9d21dea2991385b444c11978c5bc41a1bd9844
Andrea Zonca <code@andreazonca.com>
2019-04-30 13:48:56 -0700

    simplify testing for blackbody ratio

--- pysm/tests/test_dust.py
+++ pysm/tests/test_dust.py
@@ -7,32 +7,11 @@ from astropy.units import UnitsError
 
 
 def test_blackbody_ratio():
-    nu_from = 100. * units.GHz
-    nu_to = 400. * units.GHz
-    temp = 20. * units.K
+    nu_from = 100.
+    nu_to = 400.
+    temp = np.array([20.])
 
-    nu_from_unitless = 100.
-    nu_to_unitless = 400.
-    temp_unitless = 20.
-
-    nu_from_wrong_unit = 100. * units.K
-    nu_to_wrong_unit = 400. * units.K
-    temp_wrong_unit = 20. * units.s
-
-    dust.blackbody_ratio(nu_to, nu_from, temp)
-    dust.blackbody_ratio(nu_to, nu_from, temp)
-    with pytest.raises(UnitsError):
-        dust.blackbody_ratio(nu_to, nu_from_wrong_unit, temp)
-    with pytest.raises(TypeError):
-        dust.blackbody_ratio(nu_to, nu_from_unitless, temp)
-    with pytest.raises(units.UnitsError):
-        dust.blackbody_ratio(nu_to_wrong_unit, nu_from, temp)
-    with pytest.raises(TypeError):
-        dust.blackbody_ratio(nu_to_unitless, nu_from, temp)
-    with pytest.raises(units.UnitsError):
-        dust.blackbody_ratio(nu_to, nu_from, temp_wrong_unit)
-    with pytest.raises(TypeError):
-        dust.blackbody_ratio(nu_to, nu_from, temp_unitless)
+    np.testing.assert_allclose(dust.blackbody_ratio(nu_to, nu_from, temp), np.array([10.77195547]))
 
 
 @pytest.mark.parametrize("freq", [30, 100, 353])
