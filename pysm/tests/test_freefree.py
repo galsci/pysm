921cfa8a0e1237a99053cb8c519b93521fe4655c
Andrea Zonca <code@andreazonca.com>
2019-05-04 11:23:47 -0700

    use generic names in test

--- pysm/tests/test_freefree.py
+++ pysm/tests/test_freefree.py
@@ -1,21 +1,17 @@
-
-import unittest
-import numpy as np
 import pysm
 
 import pytest
 from astropy.tests.helper import assert_quantity_allclose
 
 
-#@pytest.mark.parametrize("freq", [30, 100, 353])
-@pytest.mark.parametrize("freq", [30])
+@pytest.mark.parametrize("freq", [30, 100, 353])
 @pytest.mark.parametrize("model", ["f1"])
-def test_synchrotron_model(model, freq):
+def test_model(model, freq):
 
-    synchrotron = pysm.preset_models(model, nside=64)
+    model = pysm.preset_models(model, nside=64)
 
     model_number = 4
-    synch = pysm.read_map(
+    expected_map = pysm.read_map(
         "pysm_2_test_data/check{}freef_{}p0_64.fits".format(model_number, freq),
         64,
         unit=pysm.units.uK_RJ,
@@ -23,5 +19,5 @@ def test_synchrotron_model(model, freq):
     ).reshape((1, 1, -1))
 
     assert_quantity_allclose(
-        synch, synchrotron.get_emission(freq << pysm.units.GHz), rtol=1e-5
+        expected_map, model.get_emission(freq << pysm.units.GHz), rtol=1e-5
     )
