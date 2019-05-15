7fcd425a8aa2c2b286c42474d1ea1ddf6f3c6a2f
Andrea Zonca <code@andreazonca.com>
2019-05-15 08:44:13 -0700

    import smoothing functions into base package

--- pysm/models/__init__.py
+++ pysm/models/__init__.py
@@ -1,4 +1,4 @@
 from .dust import ModifiedBlackBody, DecorrelatedModifiedBlackBody
 from .power_law import PowerLaw
-from .template import Model, read_map
+from .template import Model, read_map, apply_smoothing, mpi_smoothing
 from .spdust import SpDust, SpDustPol
