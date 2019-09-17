2f72a0a9b2fb2f08e72683553584a925e2046622
Andrea Zonca <code@andreazonca.com>
2019-09-17 10:24:37 -0700

    add CMB model c2 from template

--- pysm/models/cmb.py
+++ pysm/models/cmb.py
@@ -4,13 +4,11 @@ from .. import utils
 from .. import units as u
 from .template import Model
 
-class CMBMap(Model):
 
-    def __init__(self, map_I, map_Q, map_U, nside, map_dist=None):
+class CMBMap(Model):
+    def __init__(self, map_IQU, nside, map_dist=None):
         super().__init__(nside=nside, map_dist=map_dist)
-        self.map_I = self.read_map(map_I, unit=u.uK_CMB)
-        self.map_Q = self.read_map(map_Q, unit=u.uK_CMB)
-        self.map_U = self.read_map(map_U, unit=u.uK_CMB)
+        self.map = self.read_map(map_IQU, unit=u.uK_CMB, field=(0, 1, 2))
 
     @u.quantity_input
     def get_emission(self, freqs: u.GHz, weights=None) -> u.uK_RJ:
@@ -25,4 +23,4 @@ class CMBMap(Model):
         else:
             scaling_factor = np.trapz(convert_to_uK_RJ * weights, x=freqs.value)
 
-        return (np.array([self.map_I, self.map_Q, self.map_U]) * scaling_factor) << u.uK_RJ
+        return u.Quantity(self.map.value * scaling_factor, unit=u.uK_RJ, copy=False)
