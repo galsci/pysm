5c876eda82d44c49e9077ee16d1c1aa9c62493c6
Andrea Zonca <code@andreazonca.com>
2019-09-18 11:34:04 -0700

    implementation of lensed CMB model c1

--- pysm/models/__init__.py
+++ pysm/models/__init__.py
@@ -1,10 +1,6 @@
 from .dust import ModifiedBlackBody, DecorrelatedModifiedBlackBody
 from .power_law import PowerLaw
-from .template import (
-    Model,
-    read_map,
-    apply_smoothing_and_coord_transform,
-)
+from .template import Model, read_map, apply_smoothing_and_coord_transform
 from .spdust import SpDust, SpDustPol
 from .interpolating import InterpolatingComponent
-from .cmb import CMBMap
+from .cmb import CMBMap, CMBLensed
