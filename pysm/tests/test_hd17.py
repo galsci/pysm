22d6e0a86174612c3c9980241a3b1142c1fd02ac
Andrea Zonca <code@andreazonca.com>
2020-03-26 12:13:15 -0700

    test: unit test for HD2017

---
+++ pysm/tests/test_hd17.py
@@ -0,0 +1,26 @@
+import pytest
+import numpy as np
+import pysm
+import pysm.models.dust as dust
+import astropy.units as units
+from astropy.tests.helper import assert_quantity_allclose
+
+
+@pytest.mark.parametrize("freq", [100, 353, 900])
+@pytest.mark.parametrize("model_tag", ["d7"])
+def test_highfreq_dust_model(model_tag, freq):
+
+    model = pysm.Sky(preset_strings=[model_tag], nside=64)
+
+    expected_output = pysm.read_map(
+        "pysm_2_test_data/check_{}_{}_64.fits".format(model_tag, freq),
+        64,
+        unit="uK_RJ",
+        field=(0, 1, 2),
+    )
+
+    rtol = 1e-5
+
+    assert_quantity_allclose(
+        expected_output, model.get_emission(freq * units.GHz), rtol=rtol
+    )
