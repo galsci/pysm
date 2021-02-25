4eb7071143235182bc1d64bef8cc3ec589215c65
Andrea Zonca <code@andreazonca.com>
2021-02-25 11:16:48 -0800

    add sigmoid to module

--- docs/preprocess-templates/log_pol_tens_utils.py
+++ docs/preprocess-templates/log_pol_tens_utils.py
@@ -15,3 +15,24 @@ def log_pol_tens_to_map(log_pol_tens):
     m[0] = np.exp(log_pol_tens[0]) * np.cosh(P)
     m[1:] = log_pol_tens[1:] / P * np.exp(log_pol_tens[0]) * np.sinh(P)
     return m
+
+
+def sigmoid(x, x0, width, power=10):
+    """Sigmoid function given start point and width
+
+    Parameters
+    ----------
+    x : array
+        input x axis
+    x0 : float
+        value of x where the sigmoid starts (not the center)
+    width : float
+        width of the transition region in unit of x
+    power : float
+        tweak the steepness of the curve
+
+    Returns
+    -------
+    sigmoid : array
+        sigmoid, same length of x"""
+    return 1.0 / (1 + np.exp(-power * (x - x0 - width / 2) / width))
