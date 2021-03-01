5eab66ce24f60275977dac13e369ef96d89d3f99
Andrea Zonca <code@andreazonca.com>
2021-03-01 09:22:51 -0800

    run namaster function

---
+++ docs/preprocess-templates/utils.py
@@ -0,0 +1,52 @@
+import numpy as np
+import healpy as hp
+import pymaster as nmt
+
+
+def run_namaster(m, mask, lmax):
+    """Compute C_ell with NaMaster
+
+    Parameters
+    ----------
+    m : numpy array
+        T only or TQU HEALPix map
+    mask : numpy array
+        mask, 1D, 0 for masked pixels,
+        needs to have same Nside of the input map
+    lmax : int
+        maximum ell of the spherical harmonics transform
+
+    Returns
+    -------
+    ell : numpy array
+        array of ell from 0 to lmax (length lmax+1)
+    cl_norm : numpy array
+        ell (ell+1)/2pi factor to turn C_ell into D_ell
+        first element is set to 1
+    cl : dict of numpy arrays
+        dictionary of numpy arrays with all components
+        of the spectra, for now only II, EE, BB, no
+        cross-spectra
+    """
+    nside = hp.npix2nside(len(mask))
+
+    binning = nmt.NmtBin(nside=nside, nlb=1, lmax=lmax, is_Dell=False)
+
+    cl = {}
+
+    if len(m) == 3:
+        f_2 = nmt.NmtField(mask, m[1:].copy())  # NaMaster masks the map in-place
+        cl_namaster = nmt.compute_full_master(f_2, f_2, binning)
+        cl["EE"] = np.concatenate([[0, 0], cl_namaster[0]])
+        cl["BB"] = np.concatenate([[0, 0], cl_namaster[3]])
+    elif m.ndim == 1:
+        m = m.reshape((1, -1))
+
+    f_0 = nmt.NmtField(mask, m[:1].copy())
+    cl_namaster_I = nmt.compute_full_master(f_0, f_0, binning)
+
+    cl["II"] = np.concatenate([[0, 0], cl_namaster_I[0]])
+    ell = np.concatenate([[0, 1], binning.get_effective_ells()])
+    cl_norm = ell * (ell + 1) / np.pi / 2
+    cl_norm[0] = 1
+    return ell, cl_norm, cl
