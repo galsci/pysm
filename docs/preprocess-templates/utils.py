d90ace4f895974a72ef9490485d7ed10675c0b97
Andrea Zonca <code@andreazonca.com>
2021-03-18 09:05:44 -0700

    compute also TE

--- docs/preprocess-templates/utils.py
+++ docs/preprocess-templates/utils.py
@@ -34,15 +34,18 @@ def run_namaster(m, mask, lmax):
 
     cl = {}
 
+    f_0 = nmt.NmtField(mask, m[:1].copy())
+
     if len(m) == 3:
         f_2 = nmt.NmtField(mask, m[1:].copy())  # NaMaster masks the map in-place
         cl_namaster = nmt.compute_full_master(f_2, f_2, binning)
         cl["EE"] = np.concatenate([[0, 0], cl_namaster[0]])
         cl["BB"] = np.concatenate([[0, 0], cl_namaster[3]])
+        cl_namaster = nmt.compute_full_master(f_0, f_2, binning)
+        cl["TE"] = np.concatenate([[0, 0], cl_namaster[0]])
     elif m.ndim == 1:
         m = m.reshape((1, -1))
 
-    f_0 = nmt.NmtField(mask, m[:1].copy())
     cl_namaster_I = nmt.compute_full_master(f_0, f_0, binning)
 
     cl["II"] = np.concatenate([[0, 0], cl_namaster_I[0]])
