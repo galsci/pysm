d3c9c80d19d9a68690af41cb53829ec69376ee64
Andrea Zonca <code@andreazonca.com>
2022-02-01 09:52:38 -0800

    unit test for d9

---
+++ pysm3/tests/test_dust_pysm3.py
@@ -0,0 +1,34 @@
+import astropy.units as units
+import numpy as np
+from astropy.tests.helper import assert_quantity_allclose
+
+import pysm3
+import pysm3.models.dust as dust
+import pytest
+
+
+@pytest.mark.parametrize("freq", [30, 100, 353])
+@pytest.mark.parametrize("model_tag", ["d9"])
+def test_dust_model(model_tag, freq):
+
+    model = pysm3.Sky(preset_strings=[model_tag], nside=2048)
+
+    model_number = {"d0": 1, "d9":1, "d1": 1, "d2": 6, "d3": 9, "d6": 12}[model_tag]
+    expected_output = pysm3.read_map(
+        "pysm_2_test_data/check{}therm_{}p0_64.fits".format(model_number, freq),
+        64,
+        unit="uK_RJ",
+        field=(0, 1, 2),
+    )
+
+    # for some models we do not have tests, we compare with output from a simular model
+    # and we increase tolerance, mostly just to exercise the code.
+    rtol = {"d9": 0.9}.get(model_tag, 1e-5)
+
+    output = model.get_emission(freq * units.GHz)
+
+    assert output.mean() < 10 * u.uK_RJ
+
+    # assert_quantity_allclose(
+    #     expected_output, model.get_emission(freq * units.GHz), rtol=rtol
+    # )
