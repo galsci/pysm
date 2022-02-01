a053e26cef2e2099dba9599c9b3173a9674cad3f
Andrea Zonca <code@andreazonca.com>
2022-02-01 11:07:37 -0800

    test: d9 scaling to 857

--- pysm3/tests/test_dust_pysm3.py
+++ pysm3/tests/test_dust_pysm3.py
@@ -2,6 +2,7 @@ import astropy.units as units
 import numpy as np
 import healpy as hp
 from astropy.tests.helper import assert_quantity_allclose
+from pysm3.models.dust import blackbody_ratio
 
 import pysm3
 from pysm3 import units as u
@@ -23,3 +24,23 @@ def test_dust_model_353(model_tag):
     )
 
     assert_quantity_allclose(input_template, output)
+
+
+def test_d9_857():
+    freq = 857 * u.GHz
+
+    model = pysm3.Sky(preset_strings=["d9"], nside=2048)
+
+    output = model.get_emission(freq)
+
+    input_template = pysm3.models.read_map(
+        "dust_gnilc/gnilc_dust_template_nside{nside}.fits".format(nside=2048),
+        nside=2048,
+        field=(0, 1, 2),
+    )
+
+    freq_ref = 353 * u.GHz
+    scaling = (freq / (353 * u.GHz)) ** (1.48 - 2)
+    scaling *= blackbody_ratio(freq, freq_ref, 19.6)
+
+    assert_quantity_allclose(input_template * scaling, output, rtol=1e-6)
