0c86d0d2854d2b0fd8593858bc5c4a42d6210962
Andrea Zonca <code@andreazonca.com>
2022-02-02 21:37:24 -0800

    test: test bandpass integration normalization

--- pysm3/tests/test_utils.py
+++ pysm3/tests/test_utils.py
@@ -2,6 +2,7 @@ import numpy as np
 import pysm3
 import pysm3.units as u
 from astropy.tests.helper import assert_quantity_allclose
+from pysm3 import utils
 
 
 def test_has_polarization():
@@ -23,7 +24,7 @@ def test_bandpass_unit_conversion():
     weights = np.ones(len(freqs))
     sky = pysm3.Sky(nside=nside, preset_strings=["c2"])
     CMB_rj_int = sky.get_emission(freqs, weights)
-    CMB_thermo_int = CMB_rj_int*pysm3.utils.bandpass_unit_conversion(
+    CMB_thermo_int = CMB_rj_int * pysm3.utils.bandpass_unit_conversion(
         freqs, weights, u.uK_CMB
     )
     expected_map = pysm3.read_map(
@@ -31,3 +32,27 @@ def test_bandpass_unit_conversion():
     )
     for pol in [0, 1]:
         assert_quantity_allclose(expected_map[pol], CMB_thermo_int[pol], rtol=1e-4)
+
+
+def test_bandpass_integration_tophat():
+    input_map = np.ones(12, dtype=np.double)
+    output_map = np.zeros_like(input_map)
+    freqs = [99, 100, 101] * u.GHz
+    weights = None
+    freqs = utils.check_freq_input(freqs)
+    weights = utils.normalize_weights(freqs, weights)
+    for i, (freq, weight) in enumerate(zip(freqs, weights)):
+        utils.trapz_step_inplace(freqs, weights, i, input_map, output_map)
+    np.testing.assert_allclose(input_map, output_map)
+
+
+def test_bandpass_integration_weights():
+    input_map = np.ones(12, dtype=np.double)
+    output_map = np.zeros_like(input_map)
+    freqs = [99, 100, 101] * u.GHz
+    weights = [0.3, 1, 0.3]
+    freqs = utils.check_freq_input(freqs)
+    weights = utils.normalize_weights(freqs, weights)
+    for i, (freq, weight) in enumerate(zip(freqs, weights)):
+        utils.trapz_step_inplace(freqs, weights, i, input_map, output_map)
+    np.testing.assert_allclose(input_map, output_map)
