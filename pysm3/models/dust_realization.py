56471da7d9ae2de70ee3d60b8e4a7206c2c2c84b
Andrea Zonca <code@andreazonca.com>
2022-02-26 08:45:09 -0800

    implement reading mbb index and temp and drawing realization

--- pysm3/models/dust_realization.py
+++ pysm3/models/dust_realization.py
@@ -76,6 +76,19 @@ class ModifiedBlackBodyRealization(ModifiedBlackBody):
                 ]
             ]
             self.small_scale_cl = self.read_cl(small_scale_cl).to(u.uK_RJ ** 2)
+        self.largescale_alm_mbb_index = self.read_alm(
+            largescale_alm_mbb_index,
+            has_polarization=False,
+        ).to(u.dimensionless_unscaled)
+        self.small_scale_cl_mbb_index = self.read_cl(small_scale_cl_mbb_index).to(
+            u.dimensionless_unscaled
+        )
+        self.largescale_alm_mbb_temperature = self.read_alm(
+            largescale_alm_mbb_temperature, has_polarization=False
+        ).to(u.K)
+        self.small_scale_cl_mbb_temperature = self.read_cl(
+            small_scale_cl_mbb_temperature
+        ).to(u.K ** 2)
         self.nside = int(nside)
         (
             self.I_ref,
@@ -83,38 +96,39 @@ class ModifiedBlackBodyRealization(ModifiedBlackBody):
             self.U_ref,
             self.mbb_index,
             self.mbb_temperature,
-        ) = self.draw_realization(seed=8192)
+        ) = self.draw_realization(seeds=(8192, 777, 888))
 
-    def draw_realization(self, seed=None):
+    def draw_realization(self, seeds=None):
 
-        synalm_lmax = 8192 * 2  # for reproducibility
+        if seeds is None:
+            seeds = (None, None, None)
 
-        np.random.seed(seed)
+        synalm_lmax = 3*self.nside-1#8192 * 2  # for reproducibility
 
-        alm_log_pol_tens_small_scale = hp.synalm(
+        np.random.seed(seeds[0])
+
+        alm_small_scale = hp.synalm(
             list(self.small_scale_cl.value)
             + [np.zeros_like(self.small_scale_cl[0])] * 3,
             lmax=synalm_lmax,
             new=True,
         )
 
-        alm_log_pol_tens_small_scale = [
-            hp.almxfl(each, np.ones(3 * self.nside - 1))
-            for each in alm_log_pol_tens_small_scale
+        alm_small_scale = [
+            hp.almxfl(each, np.ones(min(synalm_lmax, 3 * self.nside - 1)))
+            for each in alm_small_scale
         ]
-        map_log_pol_tens_small_scale = hp.alm2map(
-            alm_log_pol_tens_small_scale, nside=self.nside
-        )
-        map_log_pol_tens_small_scale[0] *= hp.alm2map(
-            self.modulate_alm[0].value, self.nside
-        )
-        map_log_pol_tens_small_scale[1:] *= hp.alm2map(
-            self.modulate_alm[1].value, self.nside
-        )
+        map_small_scale = hp.alm2map(alm_small_scale, nside=self.nside)
+
+        # need later for beta, Td
+        modulate_map_I = hp.alm2map(self.modulate_alm[0].value, self.nside)
+
+        map_small_scale[0] *= modulate_map_I
+        map_small_scale[1:] *= hp.alm2map(self.modulate_alm[1].value, self.nside)
 
         I_ref, Q_ref, U_ref = (
             utils.log_pol_tens_to_map(
-                map_log_pol_tens_small_scale
+                map_small_scale
                 + hp.alm2map(
                     self.template_largescale_alm.value,
                     nside=self.nside,
@@ -124,6 +138,34 @@ class ModifiedBlackBodyRealization(ModifiedBlackBody):
         ) * 0.911  # includes color correction
         # See https://github.com/galsci/pysm/issues/99
 
-        mbb_index = 1.48 * u.dimensionless_unscaled
-        mbb_temperature = 19.6 * u.K
-        return I_ref, Q_ref, U_ref, mbb_index, mbb_temperature
+        # Fixed values for comparison with d9
+        # mbb_index = 1.48 * u.dimensionless_unscaled
+        # mbb_temperature = 19.6 * u.K
+
+        output = {}
+
+        for seed, key in zip(seeds[1:], ["mbb_index", "mbb_temperature"]):
+            np.random.seed(seed)
+            input_cl = getattr(self, f"small_scale_cl_{key}")
+            output_unit = np.sqrt(1 * input_cl.unit).unit
+            alm_small_scale = hp.synalm(
+                input_cl.value,
+                lmax=synalm_lmax,
+                new=True,
+            )
+
+            import pdb; pdb.set_trace()
+            alm_small_scale = hp.almxfl(
+                alm_small_scale, np.ones(min(3 * self.nside - 1, synalm_lmax + 1))
+            )
+            output[key] = hp.alm2map(alm_small_scale, nside=self.nside) * output_unit
+            output[key] *= modulate_map_I
+            output[key] += (
+                hp.alm2map(
+                    getattr(self, f"largescale_alm_{key}").value,
+                    nside=self.nside,
+                )
+                * output_unit
+            )
+
+        return I_ref, Q_ref, U_ref, output["mbb_index"], output["mbb_temperature"]
