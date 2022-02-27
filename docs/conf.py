71a8c5a35eba0367bc19a1cc3700aef6ae65dac5
Andrea Zonca <code@andreazonca.com>
2022-02-26 20:20:26 -0800

    exclude utils notebooks

--- docs/conf.py
+++ docs/conf.py
@@ -208,5 +208,5 @@ linkcheck_anchors = False
 #     nitpick_ignore.append((dtype, six.u(target)))
 
 extensions += ["nbsphinx", "sphinx_math_dollar", "sphinx.ext.mathjax"]
-exclude_patterns = ["_build", "**.ipynb_checkpoints"]
+exclude_patterns += ["_build", "**.ipynb_checkpoints", "**utils*"]
 nbsphinx_kernel_name = "python3"
