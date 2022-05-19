17d714b3e02f9520c720395b4442932dec1fb368
Andrea Zonca <code@andreazonca.com>
2022-05-18 20:39:37 -0700

    smooth directly the small scales

--- pysm3/tests/test_dust_gl.py
+++ pysm3/tests/test_dust_gl.py
@@ -15,26 +15,31 @@ def test_d11gl_lownside():
     nside = 512
 
     freq = 353 * u.GHz
-    beamwidth = 14 * u.arcmin # 2 pixels per beam
+    beamwidth = 14 * u.arcmin  # 2 pixels per beam
 
     d11_configuration = pysm3.sky.PRESET_MODELS["d11"].copy()
+    d11_configuration["fwhm"] = beamwidth
     del d11_configuration["class"]
     del d11_configuration["galplane_fix"]
     output_healpix = pysm3.models.ModifiedBlackBodyRealization(
-        nside=nside,  seeds=[8192, 777, 888], **d11_configuration
+        nside=nside, seeds=[8192, 777, 888], **d11_configuration
     ).get_emission(freq)
-    output_healpix = hp.smoothing(output_healpix.value, fwhm=beamwidth.to_value(u.rad)) * output_healpix.unit
+    # output_healpix = hp.smoothing(output_healpix.value, fwhm=beamwidth.to_value(u.rad)) * output_healpix.unit
     output_gl = pysm3.models.ModifiedBlackBodyRealizationGL(
-        nside=nside,  seeds=[8192, 777, 888], **d11_configuration
+        nside=nside, seeds=[8192, 777, 888], **d11_configuration
     ).get_emission(freq)
     lmax = 3 * nside - 1
     alm_dx11gl = utils.gl_map2alm(output_gl.value, lmax)
-    alm_dx11gl = hp.smoothalm(alm_dx11gl, fwhm=beamwidth.to_value(u.rad))
+    # alm_dx11gl = hp.smoothalm(alm_dx11gl, fwhm=beamwidth.to_value(u.rad))
     output_gl_to_healpix = hp.alm2map(alm_dx11gl, nside=nside) * output_gl.unit
 
     rtol = 1e-4
-    assert_quantity_allclose(output_healpix[0], output_gl_to_healpix[0], rtol=rtol, atol=5*u.uK_RJ)
-    assert_quantity_allclose(output_healpix[1:], output_gl_to_healpix[1:], rtol=rtol, atol=1*u.uK_RJ)
+    assert_quantity_allclose(
+        output_healpix[0], output_gl_to_healpix[0], rtol=rtol, atol=20 * u.uK_RJ
+    )
+    assert_quantity_allclose(
+        output_healpix[1:], output_gl_to_healpix[1:], rtol=rtol, atol=1.7 * u.uK_RJ
+    )
 
 
 # @pytest.mark.skipif(
