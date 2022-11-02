7a2fdd9217b5f113e135ab3729cedab079c2cdbb
Andrea Zonca <code@andreazonca.com>
2022-11-02 12:12:51 -0700

    test: improve test of interpolating

--- pysm3/tests/test_interpolating.py
+++ pysm3/tests/test_interpolating.py
@@ -9,19 +9,19 @@ def test_interpolating(tmp_path):
 
     nside = 4
     shape = (3, hp.nside2npix(nside))
-    hp.write_map(tmp_path / "10.fits", np.zeros(shape, dtype=np.float32))
-    hp.write_map(tmp_path / "20.fits", np.ones(shape, dtype=np.float32))
+    hp.write_map(tmp_path / "10.fits", np.ones(shape, dtype=np.float32))
+    hp.write_map(tmp_path / "20.fits", 2 * np.ones(shape, dtype=np.float32))
 
     interp = InterpolatingComponent(
         tmp_path, "uK_RJ", nside, interpolation_kind="linear"
     )
 
     interpolated_map = interp.get_emission(15 * u.GHz)
-    np.testing.assert_allclose(0.5 * np.ones(shape) * u.uK_RJ, interpolated_map)
+    np.testing.assert_allclose(1.5 * np.ones(shape) * u.uK_RJ, interpolated_map)
 
     interpolated_map = interp.get_emission(19 * u.GHz)
-    np.testing.assert_allclose(0.9 * np.ones(shape) * u.uK_RJ, interpolated_map)
+    np.testing.assert_allclose(1.9 * np.ones(shape) * u.uK_RJ, interpolated_map)
 
     # test pick one of the available maps
     interpolated_map = interp.get_emission(20 * u.GHz)
-    np.testing.assert_allclose(np.ones(shape) * u.uK_RJ, interpolated_map)
+    np.testing.assert_allclose(2 * np.ones(shape) * u.uK_RJ, interpolated_map)
