34e8f900c447ba13dcdc9e75a13eb573f6d8a8b0
Andrea Zonca <code@andreazonca.com>
2022-11-06 22:18:28 -0800

    test: revamped test of interpolating component

--- pysm3/tests/test_interpolating.py
+++ pysm3/tests/test_interpolating.py
@@ -4,17 +4,25 @@ import healpy as hp
 from .. import InterpolatingComponent
 from .. import units as u
 
+import pytest
 
-def test_interpolating(tmp_path):
+nside = 4
+shape = (3, hp.nside2npix(nside))
 
-    nside = 4
-    shape = (3, hp.nside2npix(nside))
+
+@pytest.fixture
+def interp(tmp_path):
+    """Setup the interpolating component"""
     hp.write_map(tmp_path / "10.fits", np.ones(shape, dtype=np.float32))
     hp.write_map(tmp_path / "20.fits", 2 * np.ones(shape, dtype=np.float32))
 
     interp = InterpolatingComponent(
         tmp_path, "uK_RJ", nside, interpolation_kind="linear"
     )
+    return interp
+
+
+def test_interpolating(interp):
 
     interpolated_map = interp.get_emission(15 * u.GHz)
     np.testing.assert_allclose(1.5 * np.ones(shape) * u.uK_RJ, interpolated_map)
@@ -26,16 +34,17 @@ def test_interpolating(tmp_path):
     interpolated_map = interp.get_emission(20 * u.GHz)
     np.testing.assert_allclose(2 * np.ones(shape) * u.uK_RJ, interpolated_map)
 
-def test_interpolating_bandpass_boundary(tmp_path):
 
-    nside = 4
-    shape = (3, hp.nside2npix(nside))
-    hp.write_map(tmp_path / "10.fits", np.ones(shape, dtype=np.float32))
-    hp.write_map(tmp_path / "20.fits", 2 * np.ones(shape, dtype=np.float32))
+def test_interpolating_bandpass_boundary_above(interp):
 
-    interp = InterpolatingComponent(
-        tmp_path, "uK_RJ", nside, interpolation_kind="linear"
+    interpolated_map = interp.get_emission(np.array([15, 20]) * u.GHz)
+    np.testing.assert_allclose(
+        1.82 * np.ones(shape) * u.uK_RJ, interpolated_map, rtol=1e-2
     )
 
-    interpolated_map = interp.get_emission(np.array([15, 20])*u.GHz)
-    np.testing.assert_allclose(1.5 * np.ones(shape) * u.uK_RJ, interpolated_map)
+
+def test_interpolating_bandpass_boundary_below(interp):
+    interpolated_map = interp.get_emission([10, 12] * u.GHz)
+    np.testing.assert_allclose(
+        1.118 * np.ones(shape) * u.uK_RJ, interpolated_map, rtol=1e-2
+    )
