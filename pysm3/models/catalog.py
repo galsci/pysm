ae888a22668dee18bfa5899b1bf7c3c1118ffbc8
Andrea Zonca <code@andreazonca.com>
2024-06-25 16:05:45 -0700

    feat: evaluate model 1 freq

--- pysm3/models/catalog.py
+++ pysm3/models/catalog.py
@@ -1,5 +1,6 @@
-# import numpy as np
+import numpy as np
 from numba import njit
+
 # from astropy import constants as const
 #
 # from .. import units as u
@@ -19,4 +20,40 @@ def evaluate_poly(p, x):
     N = len(p)
     for i in range(N):
         out += p[i] * x ** (N - 1 - i)
+    return out
+
+
+@njit
+def evaluate_model(freqs, weights, coeff):
+    """Integrate log polynomial model across the bandpass for
+    each source in the catalog
+
+    Parameters
+    ----------
+    freqs: np.array
+        Array of frequencies in GHz
+    weights: np.array
+        Array of relative bandpass weights already normalized
+        Same length of freqs
+    coeff: 2D np.array (n_sources, n_coeff)
+        Array of log polynomial coefficients for each source
+
+    Returns
+    -------
+    flux: np.array
+        Array of the flux of each source integrated over the band
+    """
+    n_sources = coeff.shape[0]
+    logfreqs = np.log(freqs)
+    out = np.zeros((n_sources, 1), dtype=np.float64)
+    assert len(freqs) == len(weights)
+    if len(freqs) == 1:
+        for i_source in range(n_sources):
+            out[i_source, 0] = evaluate_poly(coeff[i_source, :], logfreqs[0])
+    else:
+        flux = np.zeros(len(freqs), dtype=np.float64)
+        for i_source in range(n_sources):
+            for i_freq in range(len(freqs)):
+                flux[i_freq] = evaluate_poly(coeff[i_source, :], logfreqs[i_freq])
+            out[i_source, 0] = np.trapz(flux * weights, x=freqs)
     return out
--- pysm3/tests/test_catalog.py
+++ pysm3/tests/test_catalog.py
@@ -1,6 +1,6 @@
 from numpy.testing import assert_allclose
 
-from pysm3.models.catalog import evaluate_poly
+from pysm3.models.catalog import evaluate_poly, evaluate_model
 import numpy as np
 
 
@@ -10,3 +10,17 @@ def test_evaluate_poly():
         p = np.random.rand(N)
         x = np.random.rand(1)[0]
         assert_allclose(np.polyval(p, x), evaluate_poly(p, x))
+
+
+def test_evaluate_model_1freq_offset():
+    coeff = np.array([[0, 0, 0, 0, 3.7]])
+    freqs = np.array([100])
+    weights = np.array([0])  # not used when 1 point
+    assert evaluate_model(freqs, weights, coeff) == np.ones((1, 1)) * 3.7
+
+
+def test_evaluate_model_1freq_lin():
+    coeff = np.array([[0, 0, 0, 2, 0]])
+    freqs = np.array([np.exp(3)])
+    weights = np.array([0])
+    assert evaluate_model(freqs, weights, coeff) == np.ones((1, 1)) * 6
