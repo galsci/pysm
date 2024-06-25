b257553ee8118ffc07f5a6fa82c9b20037794346
Andrea Zonca <code@andreazonca.com>
2024-06-25 16:30:38 -0700

    test: evaluate model with bandpass

--- pysm3/tests/test_catalog.py
+++ pysm3/tests/test_catalog.py
@@ -1,3 +1,4 @@
+import pysm3 as pysm
 from numpy.testing import assert_allclose
 
 from pysm3.models.catalog import evaluate_poly, evaluate_model
@@ -12,7 +13,7 @@ def test_evaluate_poly():
         assert_allclose(np.polyval(p, x), evaluate_poly(p, x))
 
 
-def test_evaluate_model_1freq_offset():
+def test_evaluate_model_1freq_flat():
     coeff = np.array([[0, 0, 0, 0, 3.7]])
     freqs = np.array([100])
     weights = np.array([0])  # not used when 1 point
@@ -24,3 +25,22 @@ def test_evaluate_model_1freq_lin():
     freqs = np.array([np.exp(3)])
     weights = np.array([0])
     assert evaluate_model(freqs, weights, coeff) == np.ones((1, 1)) * 6
+
+
+def test_evaluate_model_2freq_flat():
+    coeff = np.array([[0, 0, 0, 0, 3.7]])
+    freqs = np.exp(np.array([3, 4]))  # ~ 20 and ~ 55 GHz
+    weights = np.array([1, 1])
+    weights = pysm.normalize_weights(freqs, weights)
+    assert evaluate_model(freqs, weights, coeff) == np.ones((1, 1)) * 3.7
+
+
+def test_evaluate_model_2freq_lin():
+    coeff = np.array([[0, 0, 0, 2, 0]])
+    freqs = np.exp(np.array([3, 4]))  # ~ 20 and ~ 55 GHz
+    weights = np.array([1, 1])
+    weights = pysm.normalize_weights(freqs, weights)
+    flux = evaluate_model(freqs, weights, coeff)[0]
+    assert flux > 6
+    assert flux < 8
+    assert flux == np.trapz(weights * np.array([6, 8]), x=freqs)
