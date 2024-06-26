fix: normalization of weights

pysm3/tests/test_catalog.py
@@ -1,4 +1,4 @@
-import pysm3 as pysm
+from astropy import units as u
 from numpy.testing import assert_allclose
 import pytest
 
@@ -32,16 +32,16 @@ def test_evaluate_model_1freq_lin():
 def test_evaluate_model_2freq_flat():
     coeff = np.array([[0, 0, 0, 0, 3.7]])
     freqs = np.exp(np.array([3, 4]))  # ~ 20 and ~ 55 GHz
-    weights = np.array([1, 1])
-    weights = pysm.normalize_weights(freqs, weights)
+    weights = np.array([1, 1], dtype=np.float64)
+    weights /= np.trapz(weights, x=freqs)
     assert evaluate_model(freqs, weights, coeff) == np.ones((1, 1)) * 3.7
 
 
 def test_evaluate_model_2freq_lin():
     coeff = np.array([[0, 0, 0, 2, 0]])
     freqs = np.exp(np.array([3, 4]))  # ~ 20 and ~ 55 GHz
-    weights = np.array([1, 1])
-    weights = pysm.normalize_weights(freqs, weights)
+    weights = np.array([1, 1], dtype=np.float64)
+    weights /= np.trapz(weights, x=freqs)
     flux = evaluate_model(freqs, weights, coeff)[0]
     assert flux > 6
     assert flux < 8
@@ -61,7 +61,7 @@ def generate_test_catalog(tmp_path_factory):
         },
         coords={
             "index": indices,
-            "power": np.arange(5),
+            "power": np.arange(5)[::-1],
             "theta": ("index", np.zeros(num_sources)),
             "phi": ("index", np.zeros(num_sources)),
         },
@@ -70,6 +70,8 @@ def generate_test_catalog(tmp_path_factory):
         catalog[field].attrs["units"] = "rad"
     for field in ["logpolycoefflux", "logpolycoefpolflux"]:
         catalog[field].attrs["units"] = "Jy"
+    catalog["logpolycoefflux"].loc[dict(index=0, power=0)] = 3.7
+    catalog["logpolycoefflux"].loc[dict(index=1, power=1)] = 2
     fn = tmp_path_factory.mktemp("data") / "test_catalog.h5"
     catalog.to_netcdf(str(fn), format="NETCDF4")  # requires netcdf4 package
     return str(fn)
@@ -78,3 +80,9 @@ def generate_test_catalog(tmp_path_factory):
 def test_catalog_class(generate_test_catalog):
     nside = 64
     catalog = PointSourceCatalog(generate_test_catalog, nside=nside)
+    freqs = np.exp(np.array([3, 4])) * u.GHz  # ~ 20 and ~ 55 GHz
+    weights = np.array([1, 1], dtype=np.float64)
+    weights /= np.trapz(weights, x=freqs.to_value(u.GHz))
+    flux = catalog.get_fluxes(freqs, weights=weights)
+    assert_allclose(flux[0], 3.7)
+    assert flux[1] == np.trapz(weights * np.array([6, 8]), x=freqs)