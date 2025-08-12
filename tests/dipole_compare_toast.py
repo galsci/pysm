101d555a0cf9953c42cef789d6c07ac0d2005684
Andrea Zonca <code@andreazonca.com>
2025-08-12 15:16:00 -0700

    feat: Split dipole component unit tests

--- tests/dipole_compare_toast.py
+++ tests/dipole_compare_toast.py
@@ -7,7 +7,7 @@ from pysm3.models.template import read_map
 
 # Reference data
 NSIDE = 256
-FREQUENCIES = [80, 90, 100, 110] * u.GHz
+FREQUENCIES = [0, 80, 90, 100, 110] * u.GHz
 REFERENCE_URLS = {
     0 * u.GHz: "test_data/dipole/dipole_nside0256_freq000GHz.fits",
     80 * u.GHz: "test_data/dipole/dipole_nside0256_freq080GHz.fits",
@@ -22,29 +22,19 @@ T_CMB = "2.7255 K_CMB"
 DIPOLE_LON = "263.986 deg"
 DIPOLE_LAT = "48.247 deg"
 
-@pytest.mark.parametrize("freq", FREQUENCIES)
-def test_dipole_component(freq):
+@pytest.mark.parametrize("freq", [80, 90, 100, 110] * u.GHz)
+def test_quadrupole_corrected_freqs(freq):
     # Load reference map
     reference_map = read_map(REFERENCE_URLS[freq], nside=NSIDE)
 
-    # Instantiate CMBDipole or CMBDipoleQuad based on frequency
-    if freq == 0 * u.GHz:
-        dipole_model = CMBDipole(
-            nside=NSIDE,
-            amp=DIPOLE_AMP,
-            T_cmb=T_CMB,
-            dip_lon=DIPOLE_LON,
-            dip_lat=DIPOLE_LAT,
-            quadrupole_correction=False,
-        )
-    else:
-        dipole_model = CMBDipoleQuad(
-            nside=NSIDE,
-            amp=DIPOLE_AMP,
-            T_cmb=T_CMB,
-            dip_lon=DIPOLE_LON,
-            dip_lat=DIPOLE_LAT,
-        )
+    # Instantiate CMBDipoleQuad (quadrupole correction enabled)
+    dipole_model = CMBDipoleQuad(
+        nside=NSIDE,
+        amp=DIPOLE_AMP,
+        T_cmb=T_CMB,
+        dip_lon=DIPOLE_LON,
+        dip_lat=DIPOLE_LAT,
+    )
 
     # Generate dipole map
     generated_map = dipole_model.get_emission(freq)
@@ -55,4 +45,32 @@ def test_dipole_component(freq):
     # Compare maps
     np.testing.assert_allclose(
         generated_map_K_CMB.value, reference_map.value, rtol=1e-6, atol=1e-6
+    )
+
+def test_no_quadrupole():
+    # Load 0 GHz reference map
+    ref_0ghz_map = read_map(REFERENCE_URLS[0 * u.GHz], nside=NSIDE)
+
+    # Instantiate CMBDipole (no quadrupole correction)
+    dipole_model = CMBDipole(
+        nside=NSIDE,
+        amp=DIPOLE_AMP,
+        T_cmb=T_CMB,
+        dip_lon=DIPOLE_LON,
+        dip_lat=DIPOLE_LAT,
+        quadrupole_correction=False,
+    )
+
+    # Generate map at 100 GHz with no quadrupole correction
+    generated_100ghz_map = dipole_model.get_emission(100 * u.GHz)
+
+    # Convert generated map to K_CMB for comparison
+    generated_100ghz_map_K_CMB = generated_100ghz_map.to(u.K_CMB, equivalencies=u.cmb_equivalencies(100 * u.GHz))
+
+    # Compare maps
+    # As discussed, this comparison is physically questionable, but requested.
+    # It will likely fail due to physical differences between a 100 GHz map
+    # and a 0 GHz reference map.
+    np.testing.assert_allclose(
+        generated_100ghz_map_K_CMB.value, ref_0ghz_map.value, rtol=1e-6, atol=1e-6
     )
